775d6c60e6a9483e430ab92cbeec27a57e9c50b5
DanYellow <jeanlouis.danielo@sfr.fr>
2015-12-16 01:42:38 +0100

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,44 @@ var fs = require("fs");
 var swig = require('gulp-swig');
 
 gulp.task('templates', function(req, res) {
-  var printscreenPath = "public/printscreens"
+  var printscreenPath = "./public/printscreens"
   var printscreensArray = [];
   var fileName = "", url = "";
 
-  fs.readdir(printscreenPath, function (err, files) {
-      if (err) {
-          throw err;
-      }
+  // Contains every files "prinscreened"
+  var files = fs.readdirSync(printscreenPath)
 
-      files.map(function (file) {
+  var isFolderExists = fs.lstatSync(printscreenPath);
+
+    // Is it a directory?
+    try {
+      if (isFolderExists.isDirectory()) {
+        files = files.map(function (file) {
           return path.join(printscreenPath, file);
-      }).filter(function (file) {
-          return fs.statSync(file).isFile();
-      }).forEach(function (file) { 
-          fileName = path.basename(file, path.extname(file))
-          url = fileName + ".html";
-          var tplToolbarObject = {name: fileName, imgPath: "printscreens/"+fileName+path.extname(file), path: url};
-          printscreensArray.push(tplToolbarObject)
-      });
+        }).filter(function (file) {
+            return fs.statSync(file).isFile();
+        }).forEach(function (file) { 
+            fileName = path.basename(file, path.extname(file))
+            url = fileName + ".html";
+            var tplToolbarObject = {name: fileName, imgPath: "printscreens/"+fileName+path.extname(file), path: url};
+            printscreensArray.push(tplToolbarObject)
+        });
+      }
+    } catch (e) {
 
-      var opts = {
+    }
+
+  var opts = {
         load_json: false,
         data: {
           printscreens: printscreensArray
         }
       };
 
-      gulp.src('./dev/views/*.html')
-        .pipe(swig(opts))
-        .pipe(swig({defaults: { cache: false }}))
-        .pipe(gulp.dest('./public/'));
-  });
+  return gulp.src('./dev/views/*.html')
+    .pipe(swig(opts))
+    .pipe(swig({defaults: { cache: false }}))
+    .pipe(gulp.dest('./public/'));
 });
 
 
@@ -67,7 +73,7 @@ gulp.task('browser-sync', ['sass', 'templates', 'fonts',], function() {
     });
 
     gulp.watch("./dev/assets/styles/**/*.scss", ['sass']);
-    gulp.watch("./dev/views/**/*.html", ['templates'])
+    gulp.watch("./dev/views/**/*.html", ['printscreens', 'templates'])
     .on('change', browserSync.reload);
 });
 
@@ -84,6 +90,40 @@ gulp.task('clean', function () {
   return gulp.src(['public'], {read: false})
     .pipe(clean());
 });
+
+
+var Pageres = require('pageres');
+var rename = require("gulp-rename");
+
+gulp.task('printscreens', function () {
+  var pageres = new Pageres({delay: 2})
+    .src('http://127.0.0.1:3000/index.html', ['1000x1000'], {
+      crop: false,
+      filename: "<%= url %>",
+      hide: ["#pages-overview-toolbar"],
+      format: "jpg"
+    })
+    .dest("./public/printscreens")
+    .run()
+    .then(() => sanitizeFilesNames());
+});
+
+// @sanitizeFilesNames
+// @desc : clean file name to be correct for the toolbar
+// @todo : remove old file
+function sanitizeFilesNames () {
+  gulp.src("./public/printscreens/**/*")
+  .pipe(rename(function (path) {
+    path.basename = path.basename.split('!')[path.basename.split('!').length-1].split('.')[0];
+    path.extname = path.extname;
+  }))
+  .pipe(gulp.dest("./public/printscreens/"));
+}
+
+
+
+
+
 
 
 gulp.task('default', ['browser-sync']);
