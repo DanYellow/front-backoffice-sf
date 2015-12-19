004aef0b065af185071ed96a8b56a8966d671d5d
DanYellow <jeanlouis.danielo@sfr.fr>
2015-12-19 23:06:09 +0100

    update toolbar

--- dev/assets/scripts/toolbar.js
+++ dev/assets/scripts/toolbar.js
@@ -2,19 +2,39 @@
 var $ = require( 'jquery' )
 var io = require('socket.io-client'); 
 
-
-var template = require("./toolbar.hbs"); 
+var template = require("./templates/toolbar.hbs");
 
 var toolBar = document.getElementById('pages-overview-toolbar');
 var toolbarManager = document.getElementById('toolbar__display-manager');
 var printscreensList = document.getElementsByClassName('printscreens-list')[0];
+var printscreensListItems = document.getElementsByClassName('printscreens-list__item');
 var generatePSBtn = document.getElementById('generatePS');
 
 var socket = io.connect('localhost:5000');
 
 toolbarManager.addEventListener('click', function (e) {
-  toolbarStatusManager()
-})
+  toolbarStatusManager();
+});
+
+var elementSelectedIndex = null;
+var getItemSelected = function getItemSelected (index) {
+  elementSelectedIndex = index;
+
+  var currentPage = {index: index}
+  window.localStorage.setItem('_toolbarCurrentPage', JSON.stringify(currentPage));
+}
+
+window.addEventListener("load", function() {
+  var elementSelectedIndex = JSON.parse(window.localStorage.getItem('_toolbarCurrentPage')).index;
+  if (!isNaN(elementSelectedIndex) ) {
+    printscreensListItems[elementSelectedIndex].classList.add("active");
+    printscreensListItems[elementSelectedIndex].scrollIntoView();
+  };
+});
+
+for (var i = 0; i < printscreensListItems.length; i++) {
+    printscreensListItems[i].addEventListener('click', getItemSelected.bind(null, i), false);
+}
 
 generatePSBtn.addEventListener('click', function (e) {
   if (!socket.connected) {
@@ -27,8 +47,10 @@ generatePSBtn.addEventListener('click', function (e) {
 
 socket.on('printScreensEnded', function(message){
   generatePSBtn.disabled = false;
-  console.log(message.printscreensDatas)
-  printscreensList.innerHTML = template({printscreensDatas: message.printscreensDatas})
+  printscreensList.innerHTML = template({printscreensDatas: message.printscreensDatas, port: window.location.port})
+  
+  
+
   //document.location.reload();
   // document.getElementById('generatePS').disabled = true
 });
